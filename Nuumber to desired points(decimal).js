ce6a20de4376519c8a16d478e1495bbac7717663
waqaralibhatti <waqar.hassan026@gmail.com>
2023-01-30 18:41:39 +0500

    Added Quiz Week 3 Tasks of MERN Bootcamp

---
+++ Nuumber to desired points(decimal).js
@@ -0,0 +1,5 @@
+function toDesiredPoints(num,points){
+    console.log(+num.toFixed(points)); // 123.13
+
+}
+toDesiredPoints(2.4444,2);
---
+++ dateof birth.js
@@ -0,0 +1,4 @@
+var days = (new Date() - new Date(2000, 1, 30))/ (1000 * 60 * 60 * 24);
+//            (today)          (then)             (milliseconds per day)
+
+console.log(days)
